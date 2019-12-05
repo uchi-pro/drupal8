ce39c7ac2a3c3bc013c7819ff5b8a1186088047e
Andrey Gladyshev <andrey@uchi.pro>
2019-12-05 06:14:57 +0000

    Добавить команду для удаления старых материалов курсов с сайта
    Команда может использоваться для удаления нод курсов, направлений обучения, типов обучения перед свежим импортом курсов из СДО.

---
+++ src/Command/ClearCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Drupal\uchi_pro\Command;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Drupal\Console\Core\Command\Command;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+
+class ClearCommand extends Command {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function configure() {
+    $this
+      ->setName('uchi_pro:clear')
+      ->setDescription('Удаляет все ноды типов обучения, направлений обучения, курсов.');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function execute(InputInterface $input, OutputInterface $output) {
+    $helper = $this->getHelper('question');
+    $question = new ConfirmationQuestion('Действительно хотите удалить все курсы, направления и типы обучения? (y/n) ', false);
+
+    if (!$helper->ask($input, $output, $question)) {
+      $output->writeln('Операция отменена.');
+      return;
+    }
+
+    $output->writeln('Удаление материалов...');
+
+    $nids = \Drupal::entityQuery('node')
+      ->condition('type', ['course', 'theme', 'training_type'], 'IN')
+      ->execute();
+
+    $storage_handler = \Drupal::entityTypeManager()->getStorage('node');
+    $entities = $storage_handler->loadMultiple($nids);
+    $storage_handler->delete($entities);
+
+    $output->writeln('Материалы удалены.');
+  }
+}
